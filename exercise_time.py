85e583b1b0f291f9b81b6dca737a152488a197f1
jooaquinn <jcenderelli@mail.austral.edu.ar>
2026-03-10 19:39:30 -0300

    Update exercise_time.py

--- exercise_time.py
+++ exercise_time.py
@@ -8,3 +8,12 @@ def time():
     3. Segundos restantes
     """
     total_segundos = 3665
+
+    
+    horas_completas = total_segundos // 3600
+    minutos_completas = (total_segundos - 3600) // 60
+    segundos_restantes = total_segundos % 60
+
+    print(horas_completas)
+    print(minutos_completas)
+    print(segundos_restantes)
